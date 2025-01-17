b263c9e06cd8dad51afd3b5a0b2cfa89fd2991d7
duhnunes <duhnunes.dev@gmail.com>
2025-01-17 09:18:28 -0300

    Delete: Old modal base and load layout to schedules

--- src/js/schedules/load.js
+++
@@ -1,7 +0,0 @@
-// Seleciona o input de data
-const selectedDate = document.getElementById("displayDate");
-
-export async function SchedulesDay() {
-  // Pega a data do input
-  const date = selectedDate.value;
-}
